c8d19def9692a1213083ea43871ff7ecd43ab45d
ewnAsikur <asikur@ewnbd.com>
2024-12-07 00:22:50 +0600

    [update] dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -15,6 +20,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding=true
     }
 
     buildTypes {
@@ -37,6 +46,12 @@ android {
 
 dependencies {
 
+    val hilt_version = "2.48.1"
+    val lifecycle_version ="2.6.2"
+    val retrofit_version ="2.9.0"
+    val coroutine_version="1.7.3"
+
+    implementation(libs.androidx.hilt.common)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +60,24 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.activity:activity-ktx:1.8.1")
+
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutine_version")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_version")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:$retrofit_version")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,28 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://maven.fabric.io/public")
+        }
+
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.dagger:dagger:2.48.1")
+        classpath("org.hamcrest:hamcrest-core:1.3")
+        classpath("androidx.test:monitor:1.6.1@aar")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+
+    }
 }
