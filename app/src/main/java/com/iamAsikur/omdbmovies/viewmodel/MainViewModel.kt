da38d78dcc18aebc858d3525610b4ff2b432ca35
ewnAsikur <asikur@ewnbd.com>
2024-12-07 14:28:35 +0600

    [update] viewmodel and resultstate for handling api response

--- app/src/main/java/com/iamAsikur/omdbmovies/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/iamAsikur/omdbmovies/viewmodel/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.iamAsikur.omdbmovies.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.iamAsikur.omdbmovies.model.MovieDetailsResponse
+import com.iamAsikur.omdbmovies.model.MovieListResponse
+import com.iamAsikur.omdbmovies.networking.ResultState
 import com.iamAsikur.omdbmovies.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +17,36 @@ class MainViewModel @Inject constructor(
     private val repository: MainRepository,
 ) : ViewModel() {
 
+    private val _movieList = MutableLiveData<ResultState<MovieListResponse>>()
+    val movieList: LiveData<ResultState<MovieListResponse>> = _movieList
 
+
+    private val _movieDetails = MutableLiveData<ResultState<MovieDetailsResponse>>()
+    val movieDetails: LiveData<ResultState<MovieDetailsResponse>> = _movieDetails
+
+
+    fun fetchMovieList(movieType: String) {
+        viewModelScope.launch {
+            _movieList.value = ResultState.Loading
+            try {
+                val response = repository.getMovieList(movieType)
+                _movieList.value = ResultState.Success(response)
+            } catch (e: Exception) {
+                _movieList.value = ResultState.Error(e.localizedMessage ?: "An error occurred")
+            }
+        }
+    }
+
+
+    fun fetchMovieDetails(movieId: String) {
+        viewModelScope.launch {
+            _movieDetails.value = ResultState.Loading
+            try {
+                val response = repository.getMovieDetails(movieId)
+                _movieDetails.value = ResultState.Success(response)
+            } catch (e: Exception) {
+                _movieDetails.value = ResultState.Error(e.localizedMessage ?: "An error occurred")
+            }
+        }
+    }
 }
