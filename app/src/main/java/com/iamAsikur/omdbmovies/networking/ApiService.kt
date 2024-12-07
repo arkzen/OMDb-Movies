6be8ee42959bda690d96356312564a2087ca305d
ewnAsikur <asikur@ewnbd.com>
2024-12-07 14:17:42 +0600

    [update] functions for calling movielist and movie content api

--- app/src/main/java/com/iamAsikur/omdbmovies/networking/ApiService.kt
+++ app/src/main/java/com/iamAsikur/omdbmovies/networking/ApiService.kt
@@ -1,7 +1,23 @@
 package com.iamAsikur.omdbmovies.networking
 
+import com.iamAsikur.omdbmovies.BuildConfig
+import com.iamAsikur.omdbmovies.model.MovieDetailsResponse
+import com.iamAsikur.omdbmovies.model.MovieListResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface ApiService {
 
+    @GET("/")
+    suspend fun getMovieList(
+        @Query("s") movieType: String,
+        @Query("apikey") apiKey: String = BuildConfig.API_KEY
+    ): MovieListResponse
 
+    @GET("/")
+    suspend fun getMovieDetails(
+        @Query("i") movieId: String,
+        @Query("apikey") apiKey: String = BuildConfig.API_KEY
+    ): MovieDetailsResponse
 }
--- app/src/main/java/com/iamAsikur/omdbmovies/repository/MainRepository.kt
+++ app/src/main/java/com/iamAsikur/omdbmovies/repository/MainRepository.kt
@@ -5,5 +5,6 @@ import com.iamAsikur.omdbmovies.networking.ApiService
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(private val apiService: ApiService) {
-
+    suspend fun getMovieList(movieType: String) = apiService.getMovieList(movieType)
+    suspend fun getMovieDetails(movieId: String) = apiService.getMovieDetails(movieId)
 }
